7f89a9c85f0adc475bd78bd23d4ecea22d323c7e
Jun-K-code <JustinCode@163.com>
2025-01-10 22:11:41 +0800

    feat: apps/backend/baidu-map-nest 修改main.ts入口文件，新增cors配置、全局注册过滤器和守卫

--- apps/backend/baidu-map-nest/src/main.ts
+++ apps/backend/baidu-map-nest/src/main.ts
@@ -1,8 +1,36 @@
-import { NestFactory } from '@nestjs/core';
+/**
+ * @description 入口文件
+ */
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+// import { LoggerMiddleware } from './middlewares/LoggerMiddleware';
+import { HttpErrorFilter } from './common/filters/ExceptionFilter';
+// import { CodeErrorFilter } from './common/filters/CodeErrorFilter';
+// import { DetailPipe } from './common/pipes/DetailPipe';
+import { AuthGuard } from './common/guards/Auth';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // app.useGlobalInterceptors(new LoggerMiddleware());
+
+  // app.useGlobalFilters;
+  app.useGlobalFilters(new HttpErrorFilter(), /* new CodeErrorFilter() */);
+
+  // app.useGlobalPipes
+  // app.useGlobalPipes(new DetailPipe());
+
+  // app.useGlobalGuards
+  const reflector = app.get(Reflector);
+  app.useGlobalGuards(new AuthGuard(reflector));
+
+  // 启用 CORS
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // 允许携带 cookie
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
