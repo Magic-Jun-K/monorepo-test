7e7b1f1b815ffecad024ac51ec6569a51e05bc7a
Jun-K-code <JustinCode@163.com>
2025-01-10 22:26:51 +0800

    feat: apps/backend/baidu-map-nest 新增pgsql数据库config配置

---
+++ apps/backend/baidu-map-nest/src/config/database.ts
@@ -0,0 +1,15 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { join } from 'node:path';
+
+export default (): TypeOrmModuleOptions => ({
+  type: 'postgres',
+  host: process.env.PG_HOST,
+  port: parseInt(process.env.PG_PORT, 10),
+  username: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  database: process.env.PG_DATABASE_NAME,
+  entities: [join(__dirname, '../', '**/**.entity{.ts,.js}')],
+  synchronize: true,
+  logging: true,
+  retryAttempts: 3,
+});
