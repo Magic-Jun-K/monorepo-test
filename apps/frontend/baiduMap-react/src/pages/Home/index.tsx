52e1f255e43f7d38c215d7792b3482131f60d335
Jun-K-code <JustinCode@163.com>
2025-02-03 23:05:40 +0800

    refactor: apps/frontend/baiduMap-react 修改Home组件

--- apps/frontend/baiduMap-react/src/pages/Home/index.tsx
+++ apps/frontend/baiduMap-react/src/pages/Home/index.tsx
@@ -1,13 +1,14 @@
 import { FC } from 'react';
+import { v4 as uuidv4 } from "uuid";
+import { Swiper } from '@eggshell/unocss-ui';
 
-import { Swiper } from '@common/ui';
 import { BASE_URL } from '@/config';
 import styles from './index.module.scss';
 
 const swiperArray = [
-  <img src={`${BASE_URL}/images/robot1.webp`} />,
-  <img src={`${BASE_URL}/images/robot2.webp`} />,
-  <img src={`${BASE_URL}/images/robot3.webp`} />
+  <img src={`${BASE_URL}/images/robot1.webp`} key={uuidv4()} />,
+  <img src={`${BASE_URL}/images/robot2.webp`} key={uuidv4()} />,
+  <img src={`${BASE_URL}/images/robot3.webp`} key={uuidv4()} />
 ];
 const Home: FC = () => {
   return (
