1b0ed38e77e78a03b1675ee392921168df6aa30d
Jun-K-code <JustinCode@163.com>
2025-03-22 22:32:00 +0800

    build: packages/antd-ui 将原来tsup改为vite打包构建

---
+++ packages/antd-ui/vite.config.ts
@@ -0,0 +1,45 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
+import { resolve } from 'node:path';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    dts({
+      include: ['src'],
+      exclude: ['src/**/*.test.tsx', 'src/**/*.test.ts'],
+      outDir: 'es',
+      rollupTypes: true, // 自动生成类型声明
+      insertTypesEntry: true // 自动插入类型声明
+    })
+  ],
+  build: {
+    outDir: 'es',
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      formats: ['es'],
+      fileName: 'index'
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'antd', '@ant-design/icons', 'styled-components', 'clsx'],
+      output: {
+        preserveModules: true, // 保留模块结构
+        // 确保外部化处理那些你不想打包进库的依赖
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          // 'antd': 'antd',
+          // '@ant-design/icons': 'AntDesignIcons',
+          // 'styled-components': 'styled',
+          'clsx': 'clsx'
+        },
+        assetFileNames: 'index.css' // 添加CSS输出文件名配置
+      }
+    },
+    cssCodeSplit: true, // 将CSS代码分割为单独的文件，确保样式被正确加载
+    minify: true, // 压缩代码
+    sourcemap: false, // 不生成sourcemap
+    emptyOutDir: true // 构建前清空输出目录
+  }
+});
