6086d0fcd0bdfe380acad5dcba3fdacef016e9ea
Jun-K-code <JustinCode@163.com>
2025-04-02 21:42:14 +0800

    refactor: apps/frontend/react-vite 抽离检查token是否过期逻辑，做为自定义hook

--- apps/frontend/react-vite/src/App.tsx
+++ apps/frontend/react-vite/src/App.tsx
@@ -1,38 +1,38 @@
-import { FC, useEffect } from 'react';
+import { FC /* useEffect */ } from 'react';
 import { RouterProvider } from 'react-router-dom';
-import { init, browserTracingIntegration } from '@sentry/react';
-import { Provider } from 'react-redux';
+// import { init, browserTracingIntegration } from '@sentry/react';
 
 import { ToastProvider } from '@/components/Toast';
 import { router } from './router/index';
 // import { initWebVitals, initPerformanceChecker } from '@/utils/performance';
-import { store } from '@/store/store';
 
 import '@/assets/css/index.scss';
 import '@/assets/css/font.scss';
 import '@eggshell/unocss-ui/build/es/index.css';
+import { useTokenExpirationCheck } from './hooks/useTokenExpirationCheck';
 
 const App: FC = () => {
-  useEffect(() => {
-    init({
-      dsn: 'https://89f7621f48c7fa98c40d0027ef411518@o4508624159244288.ingest.de.sentry.io/4508624202104912',
-      // integrations集成
-      integrations: [browserTracingIntegration()], // browserTracingIntegration是Sentry的一个跟踪集成，它会捕获浏览器的资源加载时间，Ajax请求，用户行为等信息，从而帮助开发者更好地理解应用的性能状况和用户行为模式。
-      tracesSampleRate: 1.0 // 设置跟踪采样率
-    });
+  // useEffect(() => {
+  //   init({
+  //     dsn: 'https://89f7621f48c7fa98c40d0027ef411518@o4508624159244288.ingest.de.sentry.io/4508624202104912',
+  //     // integrations集成
+  //     integrations: [browserTracingIntegration()], // browserTracingIntegration是Sentry的一个跟踪集成，它会捕获浏览器的资源加载时间，Ajax请求，用户行为等信息，从而帮助开发者更好地理解应用的性能状况和用户行为模式。
+  //     tracesSampleRate: 1.0 // 设置跟踪采样率
+  //   });
 
-    // if (process.env.NODE_ENV === 'production') {
-    // initWebVitals();
-    // initPerformanceChecker();
-    // }
-  }, []);
+  //   // if (process.env.NODE_ENV === 'production') {
+  //   // initWebVitals();
+  //   // initPerformanceChecker();
+  //   // }
+  // }, []);
+
+  // 检查token是否过期
+  useTokenExpirationCheck();
 
   return (
-    <Provider store={store}>
-      <ToastProvider>
-        <RouterProvider router={router} />
-      </ToastProvider>
-    </Provider>
+    <ToastProvider>
+      <RouterProvider router={router} />
+    </ToastProvider>
   );
-}
+};
 export default App;
