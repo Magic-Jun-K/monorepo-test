cd09d011554ac6117462f4b18f050a3e29fcce1b
Jun-K-code <JustinCode@163.com>
2025-04-02 22:01:34 +0800

    refactor: apps/frontend/react-vite 原图片渲染方式改为懒加载

--- apps/frontend/react-vite/src/pages/Home/index.tsx
+++ apps/frontend/react-vite/src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { v4 as uuidv4 } from "uuid";
+import { v4 as uuidv4 } from 'uuid';
 import { Swiper, Button } from '@eggshell/unocss-ui';
 
 import LazyImage from '@/components/LazyImage';
@@ -8,9 +8,12 @@ import { BASE_URL } from '@/config';
 import styles from './index.module.scss';
 
 const swiperArray = [
-  <img src={`${BASE_URL}/images/robot1.webp`} key={uuidv4()} />,
-  <img src={`${BASE_URL}/images/robot2.webp`} key={uuidv4()} />,
-  <img src={`${BASE_URL}/images/robot3.webp`} key={uuidv4()} />
+  // <img src={`${BASE_URL}/images/robot1.webp`} key={uuidv4()} />,
+  // <img src={`${BASE_URL}/images/robot2.webp`} key={uuidv4()} />,
+  // <img src={`${BASE_URL}/images/robot3.webp`} key={uuidv4()} />
+  <img src={`${BASE_URL}/image/public/compressed/robot1.webp`} key={uuidv4()} />,
+  <img src={`${BASE_URL}/image/public/compressed/robot2.webp`} key={uuidv4()} />,
+  <img src={`${BASE_URL}/image/public/compressed/robot3.webp`} key={uuidv4()} />
 ];
 const Home: FC = () => {
   return (
@@ -28,14 +31,22 @@ const Home: FC = () => {
         <img src={`${BASE_URL}/images/robot6.webp`} />
         <img src={`${BASE_URL}/images/robot7.webp`} />
         <img src={`${BASE_URL}/images/robot8.webp`} /> */}
-        <LazyImage src={`${BASE_URL}/images/robot1.webp`} alt="robot1" />
+        {/* <LazyImage src={`${BASE_URL}/images/robot1.webp`} alt="robot1" />
         <LazyImage src={`${BASE_URL}/images/robot2.webp`} alt="robot2" />
         <LazyImage src={`${BASE_URL}/images/robot3.webp`} alt="robot3" />
         <LazyImage src={`${BASE_URL}/images/robot4.webp`} alt="robot4" />
         <LazyImage src={`${BASE_URL}/images/robot5.webp`} alt="robot5" />
         <LazyImage src={`${BASE_URL}/images/robot6.webp`} alt="robot6" />
         <LazyImage src={`${BASE_URL}/images/robot7.webp`} alt="robot7" />
-        <LazyImage src={`${BASE_URL}/images/robot8.webp`} alt="robot8" />
+        <LazyImage src={`${BASE_URL}/images/robot8.webp`} alt="robot8" /> */}
+        <LazyImage src={`${BASE_URL}/image/public/compressed/robot1.webp`} alt="robot1" />
+        <LazyImage src={`${BASE_URL}/image/public/compressed/robot2.webp`} alt="robot2" />
+        <LazyImage src={`${BASE_URL}/image/public/compressed/robot3.webp`} alt="robot3" />
+        <LazyImage src={`${BASE_URL}/image/public/compressed/robot4.webp`} alt="robot4" />
+        <LazyImage src={`${BASE_URL}/image/public/compressed/robot5.webp`} alt="robot5" />
+        <LazyImage src={`${BASE_URL}/image/public/compressed/robot6.webp`} alt="robot6" />
+        <LazyImage src={`${BASE_URL}/image/public/compressed/robot7.webp`} alt="robot7" />
+        <LazyImage src={`${BASE_URL}/image/public/compressed/robot8.webp`} alt="robot8" />
       </div>
       <Button onClick={() => console.log('Test interaction')}>Test INP</Button>
     </>
