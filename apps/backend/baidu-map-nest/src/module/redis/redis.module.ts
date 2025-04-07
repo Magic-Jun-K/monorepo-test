71749f3d1ffe5fdc4c9f9db80edbd647c4748bea
Magic-Jun-K <JustinCode@163.com>
2025-04-07 22:32:41 +0800

    feat: apps/backend/baidu-map-nest 新增redis来存储jwt黑名单机制

---
+++ apps/backend/baidu-map-nest/src/module/redis/redis.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { RedisModule as NestRedisModule } from '@nestjs-modules/ioredis';
+
+@Module({
+  imports: [
+    NestRedisModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'single',
+        url: configService.get('REDIS_URL'),
+        host: configService.get('REDIS_HOST', 'localhost'),
+        port: configService.get<number>('REDIS_PORT', 6379),
+        password: configService.get('REDIS_PASSWORD', ''),
+        db: configService.get<number>('REDIS_DB', 0),
+      }),
+    }),
+  ],
+  exports: [NestRedisModule],
+})
+export class RedisModule {}
