6cfffb3a52007e4d50a2a678b0861923d7dc1e14
Magic-Jun-K <JustinCode@163.com>
2025-05-09 14:18:41 +0800

    chore(backend:baidu-map-nest): redis.module.ts-测试redis配置

--- apps/backend/baidu-map-nest/src/module/redis/redis.module.ts
+++ apps/backend/baidu-map-nest/src/module/redis/redis.module.ts
@@ -7,14 +7,32 @@ import { RedisModule as NestRedisModule } from '@nestjs-modules/ioredis';
     NestRedisModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'single',
-        url: configService.get('REDIS_URL'),
-        host: configService.get('REDIS_HOST', 'localhost'),
-        port: configService.get<number>('REDIS_PORT', 6379),
-        password: configService.get('REDIS_PASSWORD', ''),
-        db: configService.get<number>('REDIS_DB', 0),
-      }),
+      // useFactory: (configService: ConfigService) => ({
+      //   type: 'single',
+      //   url: configService.get('REDIS_URL'),
+      //   host: configService.get('REDIS_HOST', 'localhost'),
+      //   port: configService.get<number>('REDIS_PORT', 6379),
+      //   password: configService.get('REDIS_PASSWORD', ''),
+      //   db: configService.get<number>('REDIS_DB', 0),
+      // }),
+      useFactory: (configService: ConfigService) => {
+        console.log('测试redis config', {
+          type: 'single',
+          url: configService.get('REDIS_URL'),
+          host: configService.get('REDIS_HOST', 'localhost'),
+          port: configService.get<number>('REDIS_PORT', 6379),
+          password: configService.get('REDIS_PASSWORD', ''),
+          db: configService.get<number>('REDIS_DB', 0),
+        });
+        return {
+          type: 'single',
+          url: configService.get('REDIS_URL'),
+          host: configService.get('REDIS_HOST', 'localhost'),
+          port: configService.get<number>('REDIS_PORT', 6379),
+          password: configService.get('REDIS_PASSWORD', ''),
+          db: configService.get<number>('REDIS_DB', 0),
+        };
+      },
     }),
   ],
   exports: [NestRedisModule],
