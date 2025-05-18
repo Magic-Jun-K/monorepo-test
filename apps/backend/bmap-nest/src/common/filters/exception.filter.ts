2bfb19d00b6b41bfb83bf4aec48517b68b277df1
Magic-Jun-K <JustinCode@163.com>
2025-05-18 23:24:25 +0800

    refactor(backend:bmap-nest): 重构异常过滤器

--- apps/backend/bmap-nest/src/common/filters/exception.filter.ts
+++ apps/backend/bmap-nest/src/common/filters/exception.filter.ts
@@ -8,38 +8,47 @@ import {
   HttpException,
 } from '@nestjs/common';
 
-@Catch()
-export class HttpErrorFilter implements ExceptionFilter {
+@Catch(HttpException)
+export class HttpErrorFilter implements ExceptionFilter<HttpException> {
   catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp(); // 获取请求上下文
-    const res = ctx.getResponse();
-    const status = exception?.getStatus?.(); // 获取状态码
-    
-    let message = exception.message;
-    let detail = '异常处理';
-    
+    const response = ctx.getResponse(); // 获取请求上下文中的 response 对象
+    const request = ctx.getRequest(); // 获取请求上下文中的 request 对象
+    const status = exception.getStatus(); // 获取状态码
+
+    const exceptionRes: any = exception.getResponse(); // 获取异常响应
+    const { error, message } = exceptionRes;
+
+    // let message = exception.message;
+    // let detail = '异常处理';
+
     // Handle authentication errors(处理身份验证错误)
-    if (status === 401) {
-      const response = exception.getResponse();
-      if (typeof response === 'object' && response !== null) {
-        message = response['message'] || '认证失败';
-        detail = response['error'] || '认证失败';
-      } else {
-        message = '无效的凭证';
-        detail = '认证失败';
-      }
-    }
+    // if (status === 401) {
+    //   const response = exception.getResponse();
+    //   if (typeof response === 'object' && response !== null) {
+    //     message = response['message'] || '认证失败';
+    //     detail = response['error'] || '认证失败';
+    //   } else {
+    //     message = '无效的凭证';
+    //     detail = '认证失败';
+    //   }
+    // }
 
     // Set status code before sending response(在发送响应之前设置状态代码)
-    res.status(status).json({
-      message,
+    response.status(status).json({
       code: status,
-      detail,
-    });
-    console.log('HttpErrorFilter', { 
-      status, 
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      error,
       message,
-      response: { message, code: status, detail }
+      // detail,
+    });
+    console.log('HttpErrorFilter', {
+      code: status,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      error,
+      message,
     });
   }
 }
