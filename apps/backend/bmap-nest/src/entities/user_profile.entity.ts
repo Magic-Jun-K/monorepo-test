1ebe5f8537138d573f66d589730c134b2ee8a442
Magic-Jun-K <JustinCode@163.com>
2025-05-18 19:06:31 +0800

    feat(backend:bmap-nest): 新增user相关实体

---
+++ apps/backend/bmap-nest/src/entities/user_profile.entity.ts
@@ -0,0 +1,33 @@
+/**
+ * 用户资料实体
+ */
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  OneToOne,
+  JoinColumn,
+} from 'typeorm';
+import { UserEntity } from './user.entity';
+
+@Entity({ name: 'user_profile' })
+export class UserProfileEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ nullable: true })
+  avatar: string; // 头像
+
+  @Column({ nullable: true })
+  gender: string; // 性别
+
+  @Column({ nullable: true })
+  birthday: Date; // 生日
+
+  @Column({ nullable: true })
+  address: string; // 地址
+
+  @OneToOne(() => UserEntity, (user) => user.profile)
+  @JoinColumn({ name: 'user_id' })
+  user: UserEntity;
+}
