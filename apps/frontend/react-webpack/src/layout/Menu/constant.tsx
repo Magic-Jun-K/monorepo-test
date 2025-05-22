0bf7aeb6823e1c5c24520d5074cf65fc84b60a4b
Magic-Jun-K <JustinCode@163.com>
2025-05-22 23:30:12 +0800

    feat(frontend:react-webpack): menuLayout组件-使用Menu新的使用方式

--- apps/frontend/react-webpack/src/layout/Menu/constant.tsx
+++ apps/frontend/react-webpack/src/layout/Menu/constant.tsx
@@ -1,6 +1,41 @@
+import { Link } from 'react-router-dom';
 import type { MenuItemType } from '@eggshell/unocss-ui';
 
 export const MENU_CONFIG: MenuItemType[] = [
+  {
+    itemKey: '/',
+    label: '首页'
+  },
+  {
+    itemKey: '/form-test',
+    label: '表单测试页'
+  },
+  {
+    itemKey: '/table-test',
+    label: '表格测试页'
+  },
+  {
+    itemKey: '/baidu-map',
+    label: '百度地图'
+  }
+];
+export const TEST_MENU_CONFIG: MenuItemType[] = [
+  {
+    itemKey: '/',
+    label: <Link to="/">首页</Link> as React.ReactNode
+  },
+  {
+    itemKey: '/form-test',
+    label: <Link to="/form-test">表单测试页</Link>
+  },
+  {
+    itemKey: '/table-test',
+    label: <Link to="/table-test">表格测试页</Link>
+  },
+  {
+    itemKey: '/baidu-map',
+    label: <Link to="/baidu-map">百度地图</Link>
+  },
   {
     itemKey: 'dashboard',
     label: '仪表盘'
--- apps/frontend/react-webpack/src/layout/Menu/index.tsx
+++ apps/frontend/react-webpack/src/layout/Menu/index.tsx
@@ -1,8 +1,8 @@
 import { FC } from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import { Menu } from '@eggshell/unocss-ui';
 
-import { MENU_CONFIG } from './constant';
+import { /* MENU_CONFIG, */ TEST_MENU_CONFIG } from './constant';
 
 import styles from './index.module.scss';
 
@@ -21,19 +21,16 @@ const MainMenu: FC<MenuProps> = ({ items, className = '' }) => {
   console.log('Rendering menu items:', items);
 
   return (
+    // <ul className={styles["menu-component-ul"]}>
+    //   {items.map((item, index) => (
+    //     <li key={index}>
+    //       {/* @ts-expect-error Link 组件类型定义不兼容本用法 */}
+    //       <Link to={item.path}>{item.name}</Link>
+    //     </li>
+    //   ))}
+    // </ul>
     <nav className={`${styles['menu-component']} ${className}`}>
-      <ul>
-        {items.map((item, index) => (
-          <li key={index}>
-            {/* @ts-expect-error Link 组件类型定义不兼容本用法 */}
-            <Link to={item.path}>{item.name}</Link>
-          </li>
-        ))}
-      </ul>
-      {/* <Dropdown overlay={<Menu items={MENU_CONFIG} />}>
-        <span>测试下拉菜单</span>
-      </Dropdown> */}
-      <Menu mode="horizontal" items={MENU_CONFIG} />
+      <Menu mode="horizontal" items={TEST_MENU_CONFIG} />
     </nav>
   );
 };
