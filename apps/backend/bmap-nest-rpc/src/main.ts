45bb42064035ee3897119ab618fe25fb58a559ba
Magic-Jun-K <JustinCode@163.com>
2025-06-02 00:52:55 +0800

    refactor(backend:bmap-nest-rpc): 重构main.ts

--- apps/backend/bmap-nest-rpc/src/main.ts
+++ apps/backend/bmap-nest-rpc/src/main.ts
@@ -2,7 +2,7 @@
  * @description 入口文件
  */
 import { NestFactory } from '@nestjs/core';
-import { GrpcOptions, Transport } from '@nestjs/microservices';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { join } from 'path';
 
 import { AppModule } from './app.module';
@@ -10,12 +10,14 @@ import { HttpErrorFilter } from './common/filters/exception.filter';
 import { grpcConfig } from './config/grpc.config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // 创建 HTTP 应用程序
+  const httpApp = await NestFactory.create(AppModule);
 
-  app.useGlobalFilters(new HttpErrorFilter());
+  //  使用全局过滤器
+  httpApp.useGlobalFilters(new HttpErrorFilter());
 
   // 启用 CORS
-  app.enableCors({
+  httpApp.enableCors({
     origin: [
       'http://localhost:3000',
       'http://localhost:5173',
@@ -25,22 +27,27 @@ async function bootstrap() {
     credentials: true, // 允许携带 cookie
   });
 
-  // 配置gRPC微服务
-  app.connectMicroservice<GrpcOptions>({
-    transport: Transport.GRPC,
-    options: {
-      package: 'auth',
-      protoPath: join(process.cwd(), 'src/proto/auth.proto'),
-      ...grpcConfig,
+  // 启动 HTTP 服务
+  await httpApp.listen(process.env.PORT ?? 7000);
+  console.log(`✅ HTTP 服务已启动在端口 ${process.env.PORT ?? 7000}`);
+
+  // 创建 gRPC 微服务
+  const grpcApp = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        package: 'auth',
+        protoPath: join(process.cwd(), 'src/proto/auth.proto'),
+        ...grpcConfig,
+      },
     },
-  });
-  // 启动HTTP和gRPC微服务
-  await app.startAllMicroservices();
+  );
+
+  // 启动gRPC微服务
+  await grpcApp.listen();
   console.log('✅ gRPC微服务已启动');
 
-  // 启动 HTTP 服务
-  await app.listen(process.env.PORT ?? 7000);
-  console.log(`✅ HTTP 服务已启动在端口 ${process.env.PORT ?? 7000}`);
   console.log('🚀 所有服务启动完成');
 }
 bootstrap();
