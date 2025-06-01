cc591871b550dc8ede78624bdc5bcdc8ca19a011
Magic-Jun-K <JustinCode@163.com>
2025-06-02 00:39:46 +0800

    refactor(backend:bmap-nest-rpc): auth模块-重构auth.controller.ts
    将原来authGrpcClient改为authGrpcService

--- apps/backend/bmap-nest-rpc/src/module/auth/auth.controller.ts
+++ apps/backend/bmap-nest-rpc/src/module/auth/auth.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get, Post, Request, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 
 import { Public } from '@/common/decorators/public.decorator';
-import { AuthGrpcClient } from './auth.client';
+// import { AuthGrpcClient } from './auth.client';
+// import { AuthService } from './auth.service';
+import { AuthGrpcService } from './auth.grpc.service';
 
 // 路由拦截
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authGrpcClient: AuthGrpcClient) {}
+  constructor(
+    // private readonly authGrpcClient: AuthGrpcClient,
+    // private readonly authService: AuthService,
+    private readonly authGrpcService: AuthGrpcService,
+  ) {}
 
   /**
    * 用户登录
@@ -16,13 +22,15 @@ export class AuthController {
    */
   @Post('login')
   async login(@Body() body: { username: string; password: string }) {
-    return await this.authGrpcClient.login(body.username, body.password);
+    // return await this.authService.login(body.username, body.password);
+    return await this.authGrpcService.login(body);
   }
 
   @Post('logout')
   async logout(@Body() body: { refresh_token: string }) {
-    console.log('测试grpc logout', body);
-    return await this.authGrpcClient.logout(body.refresh_token);
+    return await this.authGrpcService.logout({
+      refreshToken: body.refresh_token,
+    });
   }
 
   /**
@@ -33,7 +41,9 @@ export class AuthController {
   @Public()
   @Post('refresh')
   async refreshToken(@Body() body: { refresh_token: string }) {
-    return await this.authGrpcClient.refreshToken(body.refresh_token);
+    return await this.authGrpcService.refreshToken({
+      refreshToken: body.refresh_token,
+    });
   }
 
   /**
@@ -41,15 +51,12 @@ export class AuthController {
    * @param req
    * @returns
    */
-  @Get('currentUser')
-  currentUser(@Request() req) {
-    return { data: req.user };
+  @Get('current-user')
+  async getCurrentUser(@Body() body: { access_token: string }) {
+    return await this.authGrpcService.getCurrentUser({
+      accessToken: body.access_token,
+    });
   }
-
-  // @Get('current-user')
-  // async getCurrentUser(@Body() body: { access_token: string }) {
-  //   return await this.authGrpcClient.getCurrentUser(body.access_token);
-  // }
 
   // 测试登录后才可访问的接口，在需要的地方使用守卫，可保证必须携带token才能访问
   // @UseGuards(AuthGuard('jwt'))
