9f8efa0b72dd3e70bb0e02e9dd2a20c5411d7373
Magic-Jun-K <JustinCode@163.com>
2025-06-02 00:48:54 +0800

    chore(backend:bmap-nest-rpc): config-修改grpc.config.ts
    查看Nest gRPC官网显示，更换端口号为50051

--- apps/backend/bmap-nest-rpc/src/config/grpc.config.ts
+++ apps/backend/bmap-nest-rpc/src/config/grpc.config.ts
@@ -1,10 +1,22 @@
 export const grpcConfig = {
-  url: '0.0.0.0:5000',
+  url: '0.0.0.0:50051',
   loader: {
     keepCase: true, // 保持字段名称的大小写
     longs: String, // 使用字符串表示长整型
     enums: String, // 使用字符串表示枚举类型
     defaults: true, // 使用默认值
     oneofs: true, // 使用 oneof
+    // keepalive: {
+    //   keepaliveTimeMs: 30000,
+    //   keepaliveTimeoutMs: 10000,
+    //   keepalivePermitWithoutCalls: true,
+    //   http2MaxPingsWithoutData: 0,
+    // },
+    // ✅ 添加调试日志
+    onModuleInit: () => {
+      console.log('🔌 gRPC服务加载模块');
+    },
   },
+  // maxConnectionAgeMs: 300000, // 5分钟
+  // maxConnectionAgeGraceMs: 10000,
 };
