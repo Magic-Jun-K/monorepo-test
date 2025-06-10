a5c73ac953c2afc58fdfd87ac79b0c0ce750e40c
Magic-Jun-K <JustinCode@163.com>
2025-06-10 23:03:34 +0800

    chore(frontend:react-vite): home页面-删除多余的注释

--- apps/frontend/react-vite/src/pages/Home/index.tsx
+++ apps/frontend/react-vite/src/pages/Home/index.tsx
@@ -10,8 +10,6 @@ import styles from './index.module.scss';
 
 const swiperArray = [
   // <img src={`${BASE_URL}/images/robot1.webp`} key={uuidv4()} />,
-  // <img src={`${BASE_URL}/images/robot2.webp`} key={uuidv4()} />,
-  // <img src={`${BASE_URL}/images/robot3.webp`} key={uuidv4()} />
   <img src={`${BASE_URL}/image/public/compressed/robot1.webp`} key={uuidv4()} />,
   <img src={`${BASE_URL}/image/public/compressed/robot2.webp`} key={uuidv4()} />,
   <img src={`${BASE_URL}/image/public/compressed/robot3.webp`} key={uuidv4()} />
@@ -25,20 +23,8 @@ const Home: FC = () => {
       <section className={styles.showImages}>
         <h2>展示图片</h2>
         {/* <img src={`${BASE_URL}/images/robot1.webp`} />
-        <img src={`${BASE_URL}/images/robot2.webp`} />
-        <img src={`${BASE_URL}/images/robot3.webp`} />
-        <img src={`${BASE_URL}/images/robot4.webp`} />
-        <img src={`${BASE_URL}/images/robot5.webp`} />
-        <img src={`${BASE_URL}/images/robot6.webp`} />
-        <img src={`${BASE_URL}/images/robot7.webp`} />
         <img src={`${BASE_URL}/images/robot8.webp`} /> */}
         {/* <LazyImage src={`${BASE_URL}/images/robot1.webp`} alt="robot1" />
-        <LazyImage src={`${BASE_URL}/images/robot2.webp`} alt="robot2" />
-        <LazyImage src={`${BASE_URL}/images/robot3.webp`} alt="robot3" />
-        <LazyImage src={`${BASE_URL}/images/robot4.webp`} alt="robot4" />
-        <LazyImage src={`${BASE_URL}/images/robot5.webp`} alt="robot5" />
-        <LazyImage src={`${BASE_URL}/images/robot6.webp`} alt="robot6" />
-        <LazyImage src={`${BASE_URL}/images/robot7.webp`} alt="robot7" />
         <LazyImage src={`${BASE_URL}/images/robot8.webp`} alt="robot8" /> */}
         <LazyImage src={`${BASE_URL}/image/public/compressed/robot1.webp`} alt="robot1" />
         <LazyImage src={`${BASE_URL}/image/public/compressed/robot2.webp`} alt="robot2" />
