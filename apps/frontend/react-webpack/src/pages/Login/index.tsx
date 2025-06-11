5932182c52d1157e9a69beddce8dd4772d949d78
Magic-Jun-K <JustinCode@163.com>
2025-06-11 13:44:20 +0800

    chore(frontend:react-webpack): login页面-去掉多余的注释

--- apps/frontend/react-webpack/src/pages/Login/index.tsx
+++ apps/frontend/react-webpack/src/pages/Login/index.tsx
@@ -224,7 +224,6 @@ const LoginContent = () => {
                   error={getFieldError('email')}
                 />
                 <div className={styles.formGroup}>
-                  {/* @ts-expect-error Controller 组件类型定义不兼容本用法 */}
                   <Controller
                     name="code"
                     control={control}
