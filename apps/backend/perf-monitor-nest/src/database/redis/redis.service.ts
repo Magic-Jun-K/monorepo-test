196ef7e1c4cb804e535fcb56e701bb94051a3a48
Magic-Jun-K <JustinCode@163.com>
2025-06-25 23:34:14 +0800

    feat(backend:perf-monitor-nest): database-新增redis服务

---
+++ apps/backend/perf-monitor-nest/src/database/redis/redis.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRedis } from '@nestjs-modules/ioredis';
+import { Redis } from 'ioredis';
+
+@Injectable()
+export class RedisService {
+  constructor(@InjectRedis() private readonly redis: Redis) {}
+
+  async ping(): Promise<string> {
+    return this.redis.ping();
+  }
+}
