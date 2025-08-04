9744c47c41ac8dd45758f52f30994be8affbbf83
Magic-Jun-K <JustinCode@163.com>
2025-08-04 23:15:28 +0800

    chore(backend:bmap-nest-rpc): grpc.config.ts - update grpc server port and clean comments

--- apps/backend/bmap-nest-rpc/src/config/grpc.config.ts
+++ apps/backend/bmap-nest-rpc/src/config/grpc.config.ts
@@ -1,18 +1,10 @@
 export const grpcConfig = {
-  url: '0.0.0.0:50051',
+  url: '0.0.0.0:7001',
   loader: {
     keepCase: true, // 保持字段名称的大小写
     longs: String, // 使用字符串表示长整型
     enums: String, // 使用字符串表示枚举类型
     defaults: true, // 使用默认值
     oneofs: true, // 使用 oneof
-    // keepalive: {
-    //   keepaliveTimeMs: 30000,
-    //   keepaliveTimeoutMs: 10000,
-    //   keepalivePermitWithoutCalls: true,
-    //   http2MaxPingsWithoutData: 0,
-    // },
   },
-  // maxConnectionAgeMs: 300000, // 5分钟
-  // maxConnectionAgeGraceMs: 10000,
 };
