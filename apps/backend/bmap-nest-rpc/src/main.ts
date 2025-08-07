dcfd9b1f3cdd1a7a5e80741bd648c7f426cfd971
Magic-Jun-K <JustinCode@163.com>
2025-08-07 22:49:51 +0800

    feat(backend:bmap-nest-rpc): main.ts - add cookie parser middleware and change REST API server port

--- apps/backend/bmap-nest-rpc/src/main.ts
+++ apps/backend/bmap-nest-rpc/src/main.ts
@@ -3,33 +3,44 @@
  */
 import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
-import { join } from 'path';
+import { Logger } from '@nestjs/common';
+import { join } from 'node:path';
+import * as cookieParser from 'cookie-parser';
 
 import { AppModule } from './app.module';
 import { HttpErrorFilter } from './common/filters/exception.filter';
 import { grpcConfig } from './config/grpc.config';
 
 async function bootstrap() {
+  const logger = new Logger('RpcServerMain');
+
   // 创建 HTTP 应用程序
   const httpApp = await NestFactory.create(AppModule);
 
-  //  使用全局过滤器
+  // 添加 cookie 解析中间件
+  httpApp.use(cookieParser());
+
+  // 使用全局过滤器
   httpApp.useGlobalFilters(new HttpErrorFilter());
 
   // 启用 CORS
   httpApp.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'http://localhost:5173',
-      'http://8.138.104.160',
-    ],
+    origin:
+      process.env.NODE_ENV === 'production'
+        ? ['http://eggshell.online', 'http://www.eggshell.online']
+        : [
+            'http://eggshell.online',
+            'http://www.eggshell.online',
+            'http://localhost:3000',
+            'http://localhost:5173',
+          ],
     methods: 'GET,POST,HEAD,PUT,PATCH,DELETE',
     credentials: true, // 允许携带 cookie
   });
 
   // 启动 HTTP 服务
-  await httpApp.listen(process.env.PORT ?? 7000);
-  console.log(`✅ HTTP 服务已启动在端口 ${process.env.PORT ?? 7000}`);
+  await httpApp.listen(process.env.PORT ?? 7100);
+  logger.log(`✅ HTTP 服务已启动在端口 ${process.env.PORT ?? 7100}`);
 
   // 创建 gRPC 微服务
   const grpcApp = await NestFactory.createMicroservice<MicroserviceOptions>(
@@ -46,8 +57,7 @@ async function bootstrap() {
 
   // 启动gRPC微服务
   await grpcApp.listen();
-  console.log('✅ gRPC微服务已启动');
-
-  console.log('🚀 所有服务启动完成');
+  logger.log('✅ gRPC微服务已启动');
+  logger.log('🚀 所有服务启动完成');
 }
 bootstrap();
