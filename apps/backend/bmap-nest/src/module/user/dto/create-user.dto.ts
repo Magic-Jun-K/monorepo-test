0671274d6bfa8cd585973357957c29ce2403bce0
Magic-Jun-K <JustinCode@163.com>
2025-08-13 19:16:23 +0800

    refactor(backend:bmap-nest): remove password field and add userType and isActive fields
    update CreateUserDto to remove password field and add userType and isActive fields

--- apps/backend/bmap-nest/src/module/user/dto/create-user.dto.ts
+++ apps/backend/bmap-nest/src/module/user/dto/create-user.dto.ts
@@ -2,9 +2,10 @@ import { z } from 'zod';
 
 export const CreateUserSchema = z.object({
   username: z.string(),
-  password: z.string(),
   email: z.string().optional(),
   phone: z.string().optional(),
+  userType: z.enum(['user', 'admin']).optional().default('user'),
+  isActive: z.boolean().optional().default(true),
 });
 
 export type CreateUserDto = z.infer<typeof CreateUserSchema>;
