cf164f6de2d01bf71f90042cdee559370c6003d2
Magic-Jun-K <JustinCode@163.com>
2025-08-13 19:40:41 +0800

    chore(backend:bmap-nest): app.controller.ts - remove unused commented code

--- apps/backend/bmap-nest/src/app.controller.ts
+++ apps/backend/bmap-nest/src/app.controller.ts
@@ -1,17 +1,8 @@
 /**
  * @description 路由处理的控制器（控制器层）
  */
-import { Controller/* , Get */ } from '@nestjs/common';
-
-// import { AppService } from './app.service';
+import { Controller } from '@nestjs/common';
 
 // 路由拦截
 @Controller()
-export class AppController {
-  // constructor(private readonly appService: AppService) {}
-
-  // @Get()
-  // getHello(): string {
-  //   return this.appService.getHello();
-  // }
-}
+export class AppController {}
