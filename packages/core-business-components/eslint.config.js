c29044f90e5dd8b20cf5fddcb6c88a3cb776197c
Magic-Jun-K <JustinCode@163.com>
2025-09-12 19:28:51 +0800

    chore(packages:core-business-components): update eslint.config.js configuration

--- packages/core-business-components/eslint.config.js
+++ packages/core-business-components/eslint.config.js
@@ -3,15 +3,25 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import { dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['lib'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parser: tseslint.parser,
+      parserOptions: {
+        project: ['./tsconfig.json', './tsconfig.app.json'],
+        tsconfigRootDir: __dirname
+      }
     },
     plugins: {
       'react-hooks': reactHooks,
@@ -26,4 +36,12 @@ export default tseslint.config(
       "@typescript-eslint/no-explicit-any": "off",
     },
   },
+  {
+    files: ['vite.config.ts'],
+    extends: [tseslint.configs.disableTypeChecked],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    }
+  }
 )
