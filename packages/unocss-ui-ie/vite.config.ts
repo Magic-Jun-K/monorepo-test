8063fdb69d526f804e381260d49345f2e77bfdb9
Magic-Jun-K <JustinCode@163.com>
2025-09-15 14:29:17 +0800

    build(packages:unocss-ui-ie): update vite.config.ts configuration file

--- packages/unocss-ui-ie/vite.config.ts
+++ packages/unocss-ui-ie/vite.config.ts
@@ -41,14 +41,14 @@ export default defineConfig({
     dts({
       include: ['src'],
       exclude: ['src/**/*.test.tsx', 'src/**/*.test.ts'],
-      outDir: 'build/es/types',
+      outDir: 'lib/es/types',
       rollupTypes: true, // 自动生成类型声明
       insertTypesEntry: true // 自动插入类型声明
     })
   ],
   build: {
     target: 'es2015', // 确保兼容 IE
-    outDir: 'build/es',
+    outDir: 'lib/es',
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       // formats: ['es', 'cjs'],
