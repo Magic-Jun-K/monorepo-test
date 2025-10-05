22c75f61abbf177f7a37c3dfe71b7b2c514d91b8
Magic-Jun-K <JustinCode@163.com>
2025-10-05 22:12:00 +0800

    style(frontend:react-webpack): update header component styling

--- apps/frontend/react-webpack/src/layout/Header/index.tsx
+++ apps/frontend/react-webpack/src/layout/Header/index.tsx
@@ -4,7 +4,7 @@ import Logout from './Logout';
 export default () => {
   return (
     <header
-      className="h-[68px] flex items-center border-b border-gray-200"
+      className="h-[68px] flex items-center shadow-md z-1"
       style={{ padding: '0px 4rem' }}
     >
       <h1 className="text-3xl font-bold" style={{ paddingRight: '1rem' }}>
