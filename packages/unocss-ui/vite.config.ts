24239cb46f7023a3042207432ab79ed84795dc9f
Magic-Jun-K <JustinCode@163.com>
2025-10-25 22:16:23 +0800

    build(packages:unocss-ui): exclude react/jsx-runtime from bundle

--- packages/unocss-ui/vite.config.ts
+++ packages/unocss-ui/vite.config.ts
@@ -27,11 +27,12 @@ export default defineConfig({
       fileName: 'index'
     },
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
       output: {
         globals: {
           react: 'React',
-          'react-dom': 'ReactDOM'
+          'react-dom': 'ReactDOM',
+          'react/jsx-runtime': 'jsxRuntime'
         },
         assetFileNames: 'index.css' // 指定输出文件的名称
       }
