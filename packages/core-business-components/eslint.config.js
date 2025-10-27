a685b59ad448c8559e5fb6cde5caa3e7c8ea5567
Magic-Jun-K <JustinCode@163.com>
2025-10-27 18:38:01 +0800

    chore(packages:core-business-components): add assembly files to eslint ignore list

--- packages/core-business-components/eslint.config.js
+++ packages/core-business-components/eslint.config.js
@@ -10,7 +10,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 export default tseslint.config(
-  { ignores: ['lib'] },
+  { ignores: ['lib', 'assembly/**/*'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
