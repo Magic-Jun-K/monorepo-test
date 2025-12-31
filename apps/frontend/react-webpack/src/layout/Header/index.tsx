95b3b0f2f6ef75c7dfd194150511ca16099b419a
Magic-Jun-K <JustinCode@163.com>
2025-12-31 19:42:08 +0800

    style(frontend:react-webpack): format code in Header/index.tsx

--- apps/frontend/react-webpack/src/layout/Header/index.tsx
+++ apps/frontend/react-webpack/src/layout/Header/index.tsx
@@ -3,10 +3,7 @@ import Logout from './Logout';
 
 export default () => {
   return (
-    <header
-      className="h-[68px] flex items-center shadow-md z-1"
-      style={{ padding: '0px 4rem' }}
-    >
+    <header className="h-[68px] flex items-center shadow-md z-1" style={{ padding: '0px 4rem' }}>
       <h1 className="text-3xl font-bold" style={{ paddingRight: '1rem' }}>
         Eggshell
       </h1>
