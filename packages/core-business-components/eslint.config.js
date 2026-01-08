51b5dcc28385425a8e91ca4c4278c60d3ee361a3
Magic-Jun-K <JustinCode@163.com>
2026-01-08 20:47:51 +0800

    chore(packages:core-business-components): update eslint.config.js configuration

--- packages/core-business-components/eslint.config.js
+++ packages/core-business-components/eslint.config.js
@@ -1,15 +1,16 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { dirname } from 'node:path'
-import { fileURLToPath } from 'node:url'
+import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-export default tseslint.config(
+export default defineConfig(
   { ignores: ['lib', 'assembly/**/*'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
@@ -20,8 +21,8 @@ export default tseslint.config(
       parser: tseslint.parser,
       parserOptions: {
         project: ['./tsconfig.json', './tsconfig.app.json'],
-        tsconfigRootDir: __dirname
-      }
+        tsconfigRootDir: __dirname,
+      },
     },
     plugins: {
       'react-hooks': reactHooks,
@@ -29,11 +30,8 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-      "@typescript-eslint/no-explicit-any": "off",
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      '@typescript-eslint/no-explicit-any': 'off',
     },
   },
   {
@@ -42,6 +40,6 @@ export default tseslint.config(
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
-    }
-  }
-)
+    },
+  },
+);
